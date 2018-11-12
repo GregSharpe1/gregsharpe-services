b4810e09a40995562ab3c11f9f1a522c3f548a3e
Greg Sharpe <me@gregsharpe.co.uk>
2018-11-12 13:25:18 +0000

    Adding aws_billing service

---
+++ aws-billing/handler.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import boto3
+import json
+import requests
+from datetime import datetime, timedelta
+
+#session = boto3.session.Session(profile_name='personal')
+client = boto3.client('ce', region_name='us-east-1')
+
+datetoday = datetime.now()
+firstdateofmonth = datetoday.replace(day=1)
+
+def get_date_today():
+    """
+        Return the date today in YEAR-MONTH-DAY format
+    """
+    time_now = datetime.now()
+    return str(time_now.strftime("%Y-%m-%d"))
+
+def get_first_of_month():
+    """
+        Return the first day of the current month in YEAR-MONTH-DAY format
+    """
+    time_now = datetime.now().replace(day=1)
+    return str(time_now.strftime("%Y-%m-%d"))
+
+def get_aws_monthly_billing():
+    """
+        Return the monthly AWS cost in USD
+    """
+
+    response = client.get_cost_and_usage(
+
+        TimePeriod={
+            'Start':str(firstdateofmonth.strftime("%Y-%m-%d")),
+            'End':  str(datetoday.strftime("%Y-%m-%d"))
+        },
+        Granularity='MONTHLY',
+        Metrics=[
+        'BlendedCost',
+        ],
+    )
+
+    return response['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
+
+def get_gbp_exchange_rate():
+    """
+        Return the current USD to GBP exchange rate.
+    """
+    response = requests.get('http://free.currencyconverterapi.com/api/v5/convert?q=USD_GBP&compact=y')
+    response = response.json()
+    return response['USD_GBP']['val']
+
+def get_aws_monthly_billing_gbp(event, context):
+    """
+        Return the current months cost from the first
+        of the current month in GBP
+    """
+
+    return float(get_aws_monthly_billing()) * get_gbp_exchange_rate()
